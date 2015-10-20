c0cc2a3dc31741108683a38012b0289519bab38d
MichaelTLux <mtlux@iastate.edu>
2015-10-20 16:27:23 -0500

    finished lab 7

--- project7/src/lab7/LineNumberer.java
+++ project7/src/lab7/LineNumberer.java
@@ -3,27 +3,33 @@ package lab7;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class LineNumberer
 {
   public static void main(String[] args) throws FileNotFoundException
   {
-	File file = new File("story.txt");
+	File file = new File("src/lab7/LineNumberer.java");
 	System.out.println(file.exists());          // true if the file exists
     System.out.println(file.getName());         // name of the file 
     System.out.println(file.getAbsolutePath()); // absolute path to the file
     System.out.println(file.length());          // size of the file
 	Scanner scanner = new Scanner(file);
     int lineCount = 1;
+    
+    //add writing to a string
+    File outFile = new File("mydocument.txt");
+    PrintWriter out = new PrintWriter(outFile);
 
     while (scanner.hasNextLine())
     {
       String line = scanner.nextLine();
-      System.out.print(lineCount + " ");
-      System.out.println(line);
+      out.print(lineCount + " ");
+      out.println(line);
       lineCount += 1;
     }
     System.out.println("Done");
     scanner.close();
+    out.close();
   }
 }
---
+++ project7/src/lab7/MyPolyPlotter.java
@@ -0,0 +1,67 @@
+package lab7;
+
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.awt.Point;
+import plotter.PolylinePlotter;
+
+public class MyPolyPlotter 
+{
+	 public static void main(String[] args) throws FileNotFoundException
+	  {
+		 PolylinePlotter plotter = new PolylinePlotter();
+		 // open the file
+		    File file = new File("test.txt");    
+		    Scanner scanner = new Scanner(file);
+		    String why="#";
+		    char commentChar= why.charAt(0);
+		    
+		 // while there are more lines...
+		    while (scanner.hasNextLine())
+		    {
+		      // get the next line
+		      String line = scanner.nextLine();
+		      line=line.trim();
+		      if (line.length()!=0 && !(line.charAt(0)==commentChar))
+		      	{
+		    	  contructTheObject(line, plotter);
+		      	}
+		    }
+
+		    
+		    
+		    
+		    
+		    
+		    scanner.close();
+	  }
+	 private static void contructTheObject(String line, PolylinePlotter plotter)
+	 {
+		 
+		 Scanner temp=new Scanner(line);
+		 int width;
+		 String color;
+		 if (temp.hasNextInt())
+		 {
+			 width=temp.nextInt();
+		 }
+		 else
+		 {
+			 width=1;
+		 }
+		 color=temp.next();
+		 int xCordinate=temp.nextInt();
+		 int yCordinate=temp.nextInt();
+		 plotter.startLine(color, new Point(xCordinate, yCordinate), width);
+		 while (temp.hasNextInt())
+		 {
+			 xCordinate=temp.nextInt();
+			 yCordinate=temp.nextInt();
+			 plotter.addPoint(new Point(xCordinate, yCordinate));
+		 }
+		 
+	 }
+
+}
